0d246b05edcff7130590637f65ed29f2f775e374
Christian Guilherme Fritsch <guilherme@opiniaoearte.com.br>
2016-07-01 18:49:48 -0300

    Adicionado comentários de código

--- sh/scripts/backup/agendarBackup.sh
+++ sh/scripts/backup/agendarBackup.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+#Insere um novo agendamento no crontab do usuário wux
+#$1 - Identificação do agendamento
+#$2 - minuto
+#$3 - hora
+#$4 - dia
+#$5 - mês
+#$6 - dia da semana
+#$7 - ip de origem
+#$8 - ip de destino
+#$9... - itens (diretórios e arquivos)
 
 path=/www/g2/sh/scripts/backup/
 script=${path}agendarBackup
-
 
 if [ -e ${script}.lock ]
 then
   echo -e "{\"exitCode\":500,\"content\":{\"message\":\"Há um processo em andamento.\"}}" >> ${script}.txt
   exit 126
 else
-  
+
   touch ${script}.lock
-  
+
   if [ -e ${script}.txt ]
-  then 
+  then
     rm ${script}.txt
   fi
   if [ -e ${script}.tmp ]
-  then 
+  then
     rm ${script}.tmp
   fi
 
 	identificacao=$1
-	minuto=`echo $2 | sed 's/A/\*/'`
+	minuto=`echo $2 | sed 's/A/\*/'` # substitui o A (all) por *
 	hora=`echo $3 | sed 's/A/\*/'`
 	dia=`echo $4 | sed 's/A/\*/'`
 	mes=`echo $5 | sed 's/A/\*/'`
@@ -30,11 +39,13 @@ else
 
 	ipOrigem=$7
 	ipDestino=$8
-	itens=`echo $* | cut -d\  -f9-`
+	itens=`echo $* | cut -d\  -f9-` # separa os itens que serão incluídos no backup
 
-  crontab -l > ${script}.tmp
+  crontab -l > ${script}.tmp # cria uma cópia da cron atual
+
+  #insere a nova linha de agendamento, que executará o script "executarBackup" com os parâmetros recebidos
   echo "$minuto $hora $dia $mes $diaSemana ${path}executarBackup.sh $ipOrigem $ipDestino $itens #${identificacao}" >> ${script}.tmp
-	crontab ${script}.tmp
+	crontab ${script}.tmp #atualiza a cron com o arquivo temporário modificado
 
 
   echo -e "{\"exitCode\":0,\"content\":{\"message\":\"Agendado com sucesso!\"}}" >> ${script}.txt
--- sh/scripts/backup/executarBackup.sh
+++ sh/scripts/backup/executarBackup.sh
@@ -10,15 +10,15 @@ then
   echo -e "{\"exitCode\":500,\"content\":{\"message\":\"Há um processo em andamento.\"}}" >> ${script}.txt
   exit 126
 else
-  
+
   touch ${script}.lock
-  
+
   if [ -e ${script}.txt ]
-  then 
+  then
     rm ${script}.txt
   fi
   if [ -e ${script}.tmp ]
-  then 
+  then
     rm ${script}.tmp
   fi
 
@@ -30,22 +30,28 @@ else
   ipOrigem=$1
   ipDestino=$2
   itens=`echo $* | cut -d\  -f3-`
- 
- 
-  ssh wux@${ipOrigem} "${path}prepararBackup.sh $itens" 
+
+  #executa a compactação no host que origina os arquivos de backup
+  ssh wux@${ipOrigem} "${path}prepararBackup.sh $itens"
+  #verifica se o "prepararBackup.sh" gerou algum arquivo de saída de erro
   houveErroPreparo=`ssh wux@${ipOrigem} "ls ${path} | grep -c \"prepararBackup.error\""`
 
 	if [ $houveErroPreparo -eq 0 ]
 	then
+    #sem erros, obtem o nome do arquivo de backup gerado
 		arquivo=`ssh wux@${ipOrigem} "ls ${path}temp/*.tar" | cut -d_ -f2`
-	
+
 		if [ $? -ne 0 ]
 		then
+      #caso não encontre nenhum arquivo na pasta temporária no host de origem, prepara a resposta de erro
     	echo -e "{\"exitCode\":500,\"content\":{\"message\":\"Ocorreu um erro ao preparar os arquivos na origem.\"}}" >> ${script}.txt
 		fi
 
   	if [ $ipOrigem != $ipDestino ]
 		then
+      #se a origem e o destino forem diferentes, executa o script que
+      #moverá os arquivos da origem para o destino
+      #temporária para  apasta final /www/g2/backups
     	${path}moverBackup.sh $ipOrigem $ipDestino
 
 			if [ $? -ne 0 ]
@@ -53,6 +59,8 @@ else
     	 echo -e "{\"exitCode\":500,\"content\":{\"message\":\"Ocorreu um erro ao mover o backup, da origem para o destino.\"}}" >> ${script}.txt
 			fi
 		else
+      #se a origem e o destino forem os mesmo, apenas move o arquivo da pasta
+      #temporária para  apasta final /www/g2/backups
     	ssh wux@${ipOrigem} "sudo mv ${path}temp/*.tar ${targetPath}"
 			if [ $? -ne 0 ]
 			then
@@ -62,9 +70,9 @@ else
 
 		fi
 
-		
+
   	houveErroMover=`ssh wux@${ipOrigem} "ls ${path} | grep -c \"moverBackup.error\""`
-  
+
 		if [ $houveErroMover -eq 0 ]
   	then
    		dataExecucao=`date +%s000`
@@ -73,19 +81,19 @@ else
 		\"exitCode\":0,
 		\"content\":
 			{
-				\"message\":\"Backup realizado com sucesso!\", 
+				\"message\":\"Backup realizado com sucesso!\",
 				\"ipOrigem\":\"$ipOrigem\",
 				\"ipDestino\":\"$ipDestino\",
-				\"arquivoGerado\":\"$arquivo\", 
+				\"arquivoGerado\":\"$arquivo\",
 				\"dataExecucao\":\"$dataExecucao\"
 			}
 	} " >> ${script}.txt
-			
+
 			echo "$dataExecucao $ipOrigem $ipDestino $arquivo" >> ${script}.log
 
 		else
  	 	echo -e "{\"exitCode\":500,\"content\":{\"message\":\"Ocorreram erros durante o process.\"}}" >> ${script}.txt
-		fi	
+		fi
   	rm ${script}.lock
 	else # se houve erro na preparacao
 		msgErro=`ssh wux@${ipOrigem} "cat ${path}prepararBackup.error"`
